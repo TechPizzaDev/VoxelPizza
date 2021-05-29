c5016a44ef1e53265cca2594d3848484cd29d963
TechPizza <piatkowski2002@gmail.com>
2021-05-29 22:43:26 +0200

    Optimized enum helpers

---
+++ VoxelPizza.Base/Utility/SpanEnumExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace VoxelPizza
+{
+    public static class SpanEnumExtensions
+    {
+        public static bool SequenceEqual<T>(this ReadOnlySpan<T> first, ReadOnlySpan<T> other)
+            where T : unmanaged, Enum
+        {
+            return MemoryMarshal.AsBytes(first).SequenceEqual(MemoryMarshal.AsBytes(other));
+        }
+
+        public static bool SequenceEqual<T>(this Span<T> first, Span<T> other)
+            where T : unmanaged, Enum
+        {
+            return SequenceEqual((ReadOnlySpan<T>)first, other);
+        }
+    }
+}

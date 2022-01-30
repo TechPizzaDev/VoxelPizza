e37fb698f62c544cd183f034f663b64dcbd52828
TechPizza <piatkowski2002@gmail.com>
2022-01-30 16:50:38 +0100

    Updated submodules with code cleanup

--- VoxelPizza.Client/Program.cs
+++ VoxelPizza.Client/Program.cs
@@ -4,12 +4,12 @@ namespace VoxelPizza.Client
 {
     public class Program
     {
-        static unsafe void Main(string[] args)
+        private static unsafe void Main(string[] args)
         {
             SDL_version version;
             Sdl2Native.SDL_GetVersion(&version);
 
-            using var app = new VoxelPizza();
+            using VoxelPizza app = new();
             app.Run();
         }
     }

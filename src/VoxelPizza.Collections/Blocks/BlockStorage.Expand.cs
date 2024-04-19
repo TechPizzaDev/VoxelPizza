abdde4fd19af7b381d386a6c1fd01bbebc3dfe78
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2024-04-19 13:43:35 +0200

    Cleaned up ref arithmetic in BlockStorage

--- src/VoxelPizza.Collections/Blocks/BlockStorage.Expand.cs
+++ src/VoxelPizza.Collections/Blocks/BlockStorage.Expand.cs
@@ -11,12 +11,13 @@ public abstract partial class BlockStorage
     [MethodImpl(MethodImplOptions.AggressiveOptimization)]
     public static void Expand8To32(ref readonly byte src, ref uint dst, nuint len)
     {
+        ref byte bSrc = ref Unsafe.AsRef(in src);
+
         if (Vector128.IsHardwareAccelerated)
         {
-            nuint i = 0;
-            for (; i + (nuint)Vector128<byte>.Count <= len; i += (nuint)Vector128<byte>.Count)
+            while (len >= (nuint)Vector128<byte>.Count)
             {
-                Vector128<byte> v1_8 = Vector128.LoadUnsafe(in src);
+                Vector128<byte> v1_8 = Vector128.LoadUnsafe(in bSrc);
 
                 (Vector128<ushort> v1_16, Vector128<ushort> v2_16) = Vector128.Widen(v1_8);
 
@@ -28,26 +29,27 @@ public abstract partial class BlockStorage
                 v3_32.StoreUnsafe(ref dst, (nuint)(2 * Vector128<uint>.Count));
                 v4_32.StoreUnsafe(ref dst, (nuint)(3 * Vector128<uint>.Count));
 
-                src = ref Unsafe.Add(ref Unsafe.AsRef(in src), Vector128<byte>.Count);
+                bSrc = ref Unsafe.Add(ref bSrc, Vector128<byte>.Count);
                 dst = ref Unsafe.Add(ref dst, Vector128<byte>.Count);
+                len -= (nuint)Vector128<byte>.Count;
             }
-            len -= i;
         }
 
         (nuint loops, nuint rem) = Math.DivRem(len, 2);
         for (nuint i = 0; i < loops; i++)
         {
-            ushort s = Unsafe.ReadUnaligned<ushort>(in src);
-            ulong d = (s & 0xFFu) | ((s & 0xFF00uL) << 24);
-            Unsafe.WriteUnaligned(ref Unsafe.As<uint, byte>(ref dst), d);
+            ushort s = Unsafe.ReadUnaligned<ushort>(in bSrc);
+            bSrc = ref Unsafe.Add(ref bSrc, sizeof(ushort));
 
-            src = ref Unsafe.Add(ref Unsafe.AsRef(in src), 2 * sizeof(byte));
+            ulong d = (s & 0xFFu) | ((s & 0xFF00uL) << 24);
+
+            Unsafe.WriteUnaligned(ref Unsafe.As<uint, byte>(ref dst), d);
             dst = ref Unsafe.Add(ref dst, 2);
         }
 
         if (rem != 0)
         {
-            dst = src;
+            dst = bSrc;
         }
     }
 
@@ -63,22 +65,27 @@ public abstract partial class BlockStorage
         Expand8To32(ref byteSrc, ref uintDst, (nuint)source.Length);
     }
 
-    public static void Expand16To32(ref readonly byte src, ref uint dst, nuint len)
+    public static void Expand16To32(ref readonly ushort src, ref uint dst, nuint len)
     {
+        ref byte bSrc = ref Unsafe.As<ushort, byte>(ref Unsafe.AsRef(in src));
+        ref byte bDst = ref Unsafe.As<uint, byte>(ref dst);
+
         (nuint loops, nuint rem) = Math.DivRem(len, 2);
         for (nuint i = 0; i < loops; i++)
         {
-            uint s = Unsafe.ReadUnaligned<uint>(in src);
-            ulong d = (s & 0xFFFFu) | ((s & 0xFFFF0000uL) << 16);
-            Unsafe.WriteUnaligned(ref Unsafe.As<uint, byte>(ref dst), d);
+            uint s = Unsafe.ReadUnaligned<uint>(in bSrc);
+            bSrc = ref Unsafe.Add(ref bSrc, sizeof(uint));
 
-            src = ref Unsafe.Add(ref Unsafe.AsRef(in src), 2 * sizeof(ushort));
-            dst = ref Unsafe.Add(ref dst, 2);
+            ulong d = (s & 0xFFFFu) | ((s & 0xFFFF0000uL) << 16);
+
+            Unsafe.WriteUnaligned(ref bDst, d);
+            bDst = ref Unsafe.Add(ref bDst, sizeof(ulong));
         }
 
         if (rem != 0)
         {
-            dst = Unsafe.ReadUnaligned<ushort>(in src);
+            ushort d = Unsafe.ReadUnaligned<ushort>(in bSrc);
+            Unsafe.WriteUnaligned(ref bDst, (uint)d);
         }
     }
 
@@ -89,36 +96,42 @@ public abstract partial class BlockStorage
             ThrowDstTooSmall();
         }
 
-        ref byte byteSrc = ref MemoryMarshal.GetReference(MemoryMarshal.AsBytes(source));
-        ref uint uintDst = ref MemoryMarshal.GetReference(destination);
-        Expand16To32(ref byteSrc, ref uintDst, (nuint)source.Length);
+        ref ushort src = ref MemoryMarshal.GetReference(source);
+        ref uint dst = ref MemoryMarshal.GetReference(destination);
+        Expand16To32(ref src, ref dst, (nuint)source.Length);
     }
 
-    public static void Expand24To32(ref readonly byte src, ref uint dst, nuint len)
+    public static void Expand24To32(ref readonly UInt24 src, ref uint dst, nuint len)
     {
+        ref byte bSrc = ref Unsafe.As<UInt24, byte>(ref Unsafe.AsRef(in src));
+        ref byte bDst = ref Unsafe.As<uint, byte>(ref dst);
+
         (nuint loops, nuint rem) = Math.DivRem(len, 4);
         for (nuint i = 0; i < loops; i++)
         {
-            ulong s01 = Unsafe.ReadUnaligned<ulong>(in src);
-            uint s12 = Unsafe.ReadUnaligned<uint>(in Unsafe.Add(ref Unsafe.AsRef(in src), sizeof(ulong)));
+            ulong s01 = Unsafe.ReadUnaligned<ulong>(in bSrc);
+            bSrc = ref Unsafe.Add(ref bSrc, sizeof(ulong));
+
+            uint s12 = Unsafe.ReadUnaligned<uint>(in bSrc);
+            bSrc = ref Unsafe.Add(ref bSrc, sizeof(int));
 
             ulong d0 = (s01 & 0x00ffffff) | (((s01 >> 24) & 0x00ffffff) << 32);
             ulong d1 = (s01 >> 48) | ((s12 & 0xff) << 16) | ((ulong)(s12 >> 8) << 32);
 
-            Unsafe.WriteUnaligned(ref Unsafe.As<uint, byte>(ref Unsafe.Add(ref dst, 0)), d0);
-            Unsafe.WriteUnaligned(ref Unsafe.As<uint, byte>(ref Unsafe.Add(ref dst, 2)), d1);
+            Unsafe.WriteUnaligned(ref bDst, d0);
+            bDst = ref Unsafe.Add(ref bDst, sizeof(ulong));
 
-            src = ref Unsafe.Add(ref Unsafe.AsRef(in src), 4 * (nuint)Unsafe.SizeOf<UInt24>());
-            dst = ref Unsafe.Add(ref dst, 4);
+            Unsafe.WriteUnaligned(ref bDst, d1);
+            bDst = ref Unsafe.Add(ref bDst, sizeof(ulong));
         }
 
         for (nuint i = 0; i < rem; i++)
         {
-            UInt24 d = Unsafe.ReadUnaligned<UInt24>(in src);
-            dst = d;
+            UInt24 d = Unsafe.ReadUnaligned<UInt24>(in bSrc);
+            bSrc = ref Unsafe.Add(ref bSrc, Unsafe.SizeOf<UInt24>());
 
-            src = ref Unsafe.Add(ref Unsafe.AsRef(in src), Unsafe.SizeOf<UInt24>());
-            dst = ref Unsafe.Add(ref dst, 1);
+            Unsafe.WriteUnaligned(ref bDst, (uint)d);
+            bDst = ref Unsafe.Add(ref bDst, Unsafe.SizeOf<uint>());
         }
     }
 
@@ -129,8 +142,8 @@ public abstract partial class BlockStorage
             ThrowDstTooSmall();
         }
 
-        ref byte byteSrc = ref MemoryMarshal.GetReference(MemoryMarshal.AsBytes(source));
-        ref uint uintDst = ref MemoryMarshal.GetReference(destination);
-        Expand24To32(ref byteSrc, ref uintDst, (nuint)source.Length);
+        ref UInt24 src = ref MemoryMarshal.GetReference(source);
+        ref uint dst = ref MemoryMarshal.GetReference(destination);
+        Expand24To32(ref src, ref dst, (nuint)source.Length);
     }
 }
--- src/VoxelPizza.Collections/Blocks/BlockStorage.Narrow.cs
+++ src/VoxelPizza.Collections/Blocks/BlockStorage.Narrow.cs
@@ -13,37 +13,32 @@ public abstract partial class BlockStorage
     {
         if (Vector128.IsHardwareAccelerated)
         {
-            nuint i = 0;
-            for (; i + (nuint)Vector128<byte>.Count <= length; i += (nuint)Vector128<byte>.Count)
+            while (length >= (nuint)Vector128<byte>.Count)
             {
                 Vector128<uint> loA = Vector128.LoadUnsafe(in src, (nuint)(0 * Vector128<uint>.Count));
                 Vector128<uint> hiA = Vector128.LoadUnsafe(in src, (nuint)(1 * Vector128<uint>.Count));
-
                 Vector128<uint> loB = Vector128.LoadUnsafe(in src, (nuint)(2 * Vector128<uint>.Count));
                 Vector128<uint> hiB = Vector128.LoadUnsafe(in src, (nuint)(3 * Vector128<uint>.Count));
-                
+                src = ref Unsafe.Add(ref Unsafe.AsRef(in src), Vector128<byte>.Count);
+
                 Vector128<short> lo = V128Helper.NarrowSaturate(loA.AsInt32(), hiA.AsInt32());
                 Vector128<short> hi = V128Helper.NarrowSaturate(loB.AsInt32(), hiB.AsInt32());
                 Vector128<byte> value = V128Helper.NarrowSaturate(lo, hi).AsByte();
 
                 value.StoreUnsafe(ref dst);
-
-                src = ref Unsafe.Add(ref Unsafe.AsRef(in src), Vector128<byte>.Count);
                 dst = ref Unsafe.Add(ref dst, Vector128<byte>.Count);
+
+                length -= (nuint)Vector128<byte>.Count;
             }
-            length -= i;
         }
 
         for (nuint i = 0; i < length; i++)
         {
-            uint value = src;
-            dst = (byte)value;
-
-            src = ref Unsafe.Add(ref Unsafe.AsRef(in src), 1);
-            dst = ref Unsafe.Add(ref dst, 1);
+            uint value = Unsafe.Add(ref Unsafe.AsRef(in src), i);
+            Unsafe.Add(ref dst, i) = (byte)value;
         }
     }
-    
+
     public static void Narrow(ReadOnlySpan<uint> source, Span<byte> destination)
     {
         if (source.Length > destination.Length)
@@ -59,13 +54,13 @@ public abstract partial class BlockStorage
     [MethodImpl(MethodImplOptions.AggressiveOptimization)]
     public static void Narrow(ref readonly uint src, ref ushort dst, nuint length)
     {
-        nuint i = 0;
         if (Vector128.IsHardwareAccelerated)
         {
-            for (; i + (nuint)Vector128<ushort>.Count <= length; i += (nuint)Vector128<ushort>.Count)
+            while (length >= (nuint)Vector128<ushort>.Count)
             {
                 Vector128<uint> lo = Vector128.LoadUnsafe(in src);
                 src = ref Unsafe.Add(ref Unsafe.AsRef(in src), Vector128<uint>.Count);
+
                 Vector128<uint> hi = Vector128.LoadUnsafe(in src);
                 src = ref Unsafe.Add(ref Unsafe.AsRef(in src), Vector128<uint>.Count);
 
@@ -73,10 +68,12 @@ public abstract partial class BlockStorage
 
                 value.StoreUnsafe(ref dst);
                 dst = ref Unsafe.Add(ref dst, Vector128<ushort>.Count);
+
+                length -= (nuint)Vector128<ushort>.Count;
             }
         }
 
-        for (; i < length; i++)
+        for (nuint i = 0; i < length; i++)
         {
             uint value = Unsafe.Add(ref Unsafe.AsRef(in src), i);
             Unsafe.Add(ref dst, i) = (ushort)value;

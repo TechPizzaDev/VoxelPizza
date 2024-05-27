9c32c1ff2947665ad485d3ffe85763fafb0a336b
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2024-05-27 20:15:01 +0200

    Use FastNoise for WavesTerrain

--- src/VoxelPizza.Client/Voxels/WavesTerrainGenerator.cs
+++ src/VoxelPizza.Client/Voxels/WavesTerrainGenerator.cs
@@ -1,4 +1,10 @@
 using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
+using SharpFastNoise2;
+using SharpFastNoise2.Functions;
 using VoxelPizza.Collections.Blocks;
 using VoxelPizza.Memory;
 
@@ -18,7 +24,7 @@ public class WavesTerrainGenerator : TerrainGenerator
         }
         return true;
     }
-    
+
     public override ChunkTicket CreateTicket(ValueArc<Chunk> chunk)
     {
         return new WavesTerrainTicket(chunk.Wrap());
@@ -45,52 +51,162 @@ public class WavesTerrainGenerator : TerrainGenerator
             Chunk chunk = GetChunk().Get();
             ChunkPosition chunkPos = chunk.Position;
 
-            BlockPosition blockPos = chunkPos.ToBlock();
+            BlockPosition blockOrigin = chunkPos.ToBlock();
             BlockStorage blockStorage = chunk.GetBlockStorage();
 
-            Span<uint> layerTmp = stackalloc uint[Chunk.Width * Chunk.Depth];
+            Span<int> layerBuffer = stackalloc int[Chunk.Width * Chunk.Depth];
 
             for (int y = 0; y < Chunk.Height; y++)
             {
-                float blockY = blockPos.Y + y;
-
                 if (IsStopRequested)
                 {
                     return State;
                 }
 
-                for (int z = 0; z < Chunk.Depth; z++)
+                if (Avx2Functions.IsSupported)
                 {
-                    float blockZ = blockPos.Z + z;
-                    float cos = 64 * (MathF.Cos(blockZ / 16f) + 1) * 0.5f;
-
-                    if (cos * 0.5f >= blockY)
-                    {
-                        for (int x = 0; x < Chunk.Width; x++)
-                        {
-                            layerTmp[x + z * Chunk.Width] = 1;
-                        }
-                        continue;
-                    }
-
-                    for (int x = 0; x < Chunk.Width; x++)
-                    {
-                        float blockX = blockPos.X + x;
-                        float sin = 64 * (MathF.Sin(blockX / 16f) + 1) * 0.5f;
-
-                        uint v = 0;
-                        if ((sin + cos) * 0.5f >= blockY)
-                        {
-                            v = 1;
-                        }
-                        layerTmp[x + z * Chunk.Width] = v;
-                    }
+                    GenLayerBody<Vector256<uint>, Vector256<float>, Vector256<int>, Avx2Functions> body = new(blockOrigin, y);
+                    RunBody(body, layerBuffer);
+                }
+                else
+                {
+                    GenLayerBody<Vector128<uint>, Vector128<float>, Vector128<int>, Sse2Functions> body = new(blockOrigin, y);
+                    RunBody(body, layerBuffer);
                 }
 
-                blockStorage.SetBlockLayer(y, layerTmp);
+                blockStorage.SetBlockLayer(y, MemoryMarshal.Cast<int, uint>(layerBuffer));
             }
 
             return TransitionState(GeneratorState.Complete);
+        }
+
+        private struct GenLayerBody<m32, f32, i32, F>(BlockPosition origin, int y) : IGenBody<int>
+            where m32 : unmanaged
+            where f32 : unmanaged
+            where i32 : unmanaged
+            where F : IFunctionList<m32, f32, i32, F>
+        {
+            public static int Length => Chunk.Depth;
+            public static int Stride => Chunk.Width;
+            public static int StepSize => F.Count;
+
+            public BlockPosition origin = origin;
+            public f32 vBlockY = F.Broad((float)(origin.Y + y));
+            public f32 vBlockZ = F.Add(F.Broad(origin.Z / 16f), F.Mul(F.Incremented_f32(), F.Broad(1f / 16f)));
+
+            public readonly void Run(Span<int> rows) => Run(F.Count, rows);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public readonly void Run(int count, Span<int> rows)
+            {
+                Debug.Assert((uint)count <= (uint)F.Count);
+
+                f32 cos = F.Mul(F.Add(Utils<m32, f32, i32, F>.Cos_f32(vBlockZ), F.Broad(1f)), F.Broad(32f));
+
+                m32 cond = F.GreaterThanOrEqual(F.Mul(cos, F.Broad(0.5f)), vBlockY);
+                if (count == F.Count && F.AllMask(cond))
+                {
+                    rows.Fill(1);
+                }
+                else
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        Span<int> row = rows.Slice(i * Chunk.Width, Chunk.Width);
+
+                        f32 rowCos = F.Broad(F.Extract(cos, i));
+                        GenRowBody<m32, f32, i32, F> rowBody = new(rowCos, vBlockY, origin.X);
+                        RunBody(rowBody, row);
+                    }
+                }
+            }
+
+            public void Step() => Step(F.Count);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void Step(int count)
+            {
+                vBlockZ = F.Add(vBlockZ, F.Broad(count / 16f));
+            }
+        }
+
+        private struct GenRowBody<m32, f32, i32, F>(f32 cos, f32 blockY, float blockX) : IGenBody<int>
+            where m32 : unmanaged
+            where f32 : unmanaged
+            where i32 : unmanaged
+            where F : IFunctionList<m32, f32, i32, F>
+        {
+            public static int Length => Chunk.Width;
+            public static int Stride => 1;
+            public static int StepSize => F.Count;
+
+            public f32 vBlockX = F.Add(F.Broad(blockX / 16f), F.Mul(F.Incremented_f32(), F.Broad(1f / 16f)));
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            private readonly i32 Run()
+            {
+                f32 vSin = Utils<m32, f32, i32, F>.Sin_f32(vBlockX);
+                f32 vScaledSin = F.Mul(F.Add(vSin, F.Broad((float)1)), F.Broad(32f));
+
+                m32 cond = F.GreaterThanOrEqual(F.Mul(F.Add(vScaledSin, cos), F.Broad(0.5f)), blockY);
+                i32 v = F.Select(cond, F.Broad(1), F.Broad(0));
+                return v;
+            }
+
+            public readonly void Run(Span<int> row)
+            {
+                i32 v = Run();
+                F.Store(row, v);
+            }
+
+            public readonly void Run(int count, Span<int> row)
+            {
+                i32 v = Run();
+                for (int i = 0; i < count; i++)
+                {
+                    row[i] = F.Extract(v, i);
+                }
+            }
+
+            public void Step() => Step(F.Count);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void Step(int count)
+            {
+                vBlockX = F.Add(vBlockX, F.Broad(count / 16f));
+            }
+        }
+
+        private static void RunBody<T, E>(T body, Span<E> destination)
+            where T : IGenBody<E>
+        {
+            int i = 0;
+            for (; i + T.StepSize <= T.Length; i += T.StepSize)
+            {
+                Span<E> span = destination.Slice(i * T.Stride, T.Stride * T.StepSize);
+                body.Run(span);
+                body.Step();
+            }
+
+            int tailCount = T.Length - i;
+            if (tailCount > 0)
+            {
+                Span<E> tailSpan = destination.Slice(i * T.Stride, T.Stride * tailCount);
+                body.Run(tailCount, tailSpan);
+            }
+        }
+
+        private interface IGenBody<T>
+        {
+            static abstract int Length { get; }
+            static abstract int Stride { get; }
+            static abstract int StepSize { get; }
+
+            void Run(Span<T> destination);
+            void Run(int count, Span<T> destination);
+
+            void Step();
+            void Step(int count);
         }
     }
 }

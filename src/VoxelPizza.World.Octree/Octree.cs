339e0d1720e9d6b33b842038f6f079429fc1624b
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2024-06-05 13:50:42 +0200

    Store Octree leaves as contiguous array

--- src/VoxelPizza.World.Octree/Octree.cs
+++ src/VoxelPizza.World.Octree/Octree.cs
@@ -1,15 +1,22 @@
 using System;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 namespace VoxelPizza.World.Octree;
 
+/// <summary>
+/// </summary>
+/// <remarks>
+/// Heavily based on <see href="https://github.com/mwarning/SimpleOctree" />, 
+/// modified to not store leaves as individual nodes.
+/// </remarks>
+/// <typeparam name="B">Value stored in each branch.</typeparam>
+/// <typeparam name="L">Value stored in each leaf.</typeparam>
 public class Octree<B, L>
 {
     private readonly int _depth;
 
-    private Node? _root;
+    private Branch? _root;
     private int _branchCount;
     private int _leafCount;
 
@@ -18,17 +25,22 @@ public class Octree<B, L>
     /// </summary>
     public int Depth => _depth;
 
-    /// <summary>
-    /// Size of the bounding box.
-    /// Always a power of two.
-    /// </summary>
-    public int Width => 1 << _depth;
-
     public Octree(int depth)
     {
         ArgumentOutOfRangeException.ThrowIfNegative(depth);
 
         _depth = depth;
+    }
+    
+    /// <summary>
+    /// Size of the bounding box, serving as the exclusive bound for coordinates.
+    /// </summary>
+    /// <remarks>
+    /// Always a power of two.
+    /// </remarks>
+    public int GetWidth()
+    {
+        return 1 << (_depth + 1);
     }
 
     /// <summary>
@@ -36,112 +48,151 @@ public class Octree<B, L>
     /// </summary>
     public int GetMaxCapacity()
     {
-        int w = Width;
+        int w = GetWidth();
         return w * w * w;
     }
 
     /// <summary>
-    /// Get a <see cref="Leaf"/> at the given position if it exists.
+    /// Get a <see cref="Leaf"/> at the given position if the branch exists.
     /// </summary>
-    /// <returns>A <see cref="Leaf"/> if it exists, otherwise <see langword="null"/>.</returns>
-    public Leaf? Get(int x, int y, int z)
+    /// <returns>The <see cref="Leaf"/> if the branch exists, otherwise an empty <see cref="Leaf"/>.</returns>
+    public Leaf GetLeaf(int x, int y, int z)
     {
-        Node? node = _root;
-        int size = Width;
+        uint width = (uint)GetWidth();
+        Debug.Assert((uint)x < width);
+        Debug.Assert((uint)y < width);
+        Debug.Assert((uint)z < width);
 
-        Debug.Assert(x < size);
-        Debug.Assert(y < size);
-        Debug.Assert(z < size);
+        Branch? branch = _root;
+        int mask = 1 << _depth;
 
-        while (size != 1 && node != null)
+        while (mask != 1 && branch != null)
         {
-            size /= 2;
-            int index = GetIndex(x, y, z, size);
-            node = Unsafe.As<Branch>(node).GetNode(index);
+            Debug.Assert(branch is NestBranch);
+
+            mask >>>= 1;
+            int index = GetIndex(x, y, z, mask);
+            branch = Unsafe.As<NestBranch>(branch).GetBranch(index);
         }
 
-        return Unsafe.As<Leaf>(node);
+        if (branch == null)
+        {
+            return new Leaf();
+        }
+        Debug.Assert(branch is LeafBranch);
+
+        int leafIndex = GetIndex(x, y, z, 1);
+        return new Leaf(Unsafe.As<LeafBranch>(branch), leafIndex);
     }
 
     /// <summary>
-    /// Get or add a <see cref="Leaf "/> at the given position.
+    /// Get or add a <see cref="Leaf"/> at the given position.
     /// </summary>
     /// <returns>The <see cref="Leaf"/>.</returns>
-    public Leaf GetOrAdd(int x, int y, int z)
+    public Leaf GetOrAddLeaf(int x, int y, int z)
     {
-        ref Node? node = ref _root;
-        int depth = _depth;
-        int width = Width;
+        uint width = (uint)GetWidth();
+        Debug.Assert((uint)x < width);
+        Debug.Assert((uint)y < width);
+        Debug.Assert((uint)z < width);
 
-        Debug.Assert(x < width);
-        Debug.Assert(y < width);
-        Debug.Assert(z < width);
+        ref Branch? branch = ref _root;
+        int depth = _depth;
 
         while (depth > 0)
         {
-            if (node == null)
+            if (branch == null)
             {
-                node = AllocBranch();
+                branch = AllocNestBranch();
             }
+            Debug.Assert(branch is NestBranch);
 
-            --depth;
+            depth--;
 
-            int size = 1 << depth;
-            int index = GetIndex(x, y, z, size);
-            node = ref Unsafe.As<Branch>(node).GetNode(index);
+            int mask = 1 << depth;
+            int index = GetIndex(x, y, z, mask);
+            branch = ref Unsafe.As<NestBranch>(branch).GetBranch(index);
         }
 
-        if (node == null)
+        if (branch == null)
         {
             Debug.Assert(depth == 0);
-            node = new Leaf();
+            branch = new LeafBranch();
             _leafCount++;
         }
+        Debug.Assert(branch is LeafBranch);
 
-        return Unsafe.As<Leaf>(node);
+        int leafIndex = GetIndex(x, y, z, 1);
+        return new Leaf(Unsafe.As<LeafBranch>(branch), leafIndex);
     }
 
     [MethodImpl(MethodImplOptions.NoInlining)]
-    private Branch AllocBranch()
+    private NestBranch AllocNestBranch()
     {
-        Branch branch = new();
+        NestBranch branch = new();
         _branchCount++;
         return branch;
     }
 
-    private static int GetIndex(int x, int y, int z, int size)
+    private static int GetIndex(int x, int y, int z, int mask)
     {
-        // The nth bit of x, y and z is encoded in the index.
+        // The Nth bit of x, y and z is encoded in the index.
         // Since size is always a power of two, size has always
-        // only one bit set and it is used as bit mask to check the nth bit.
+        // only one bit set and it is used as bit mask to check the Nth bit.
 
         return
-            ((x & size) != 0 ? 1 : 0) * 1 +
-            ((z & size) != 0 ? 1 : 0) * 2 +
-            ((y & size) != 0 ? 1 : 0) * 4;
+            ((x & mask) != 0 ? 1 : 0) * 1 +
+            ((z & mask) != 0 ? 1 : 0) * 2 +
+            ((y & mask) != 0 ? 1 : 0) * 4;
     }
 
-    public abstract class Node
+    public readonly struct Leaf(LeafBranch parent, int index) : IEquatable<Leaf>
     {
+        public LeafBranch Parent { get; } = parent;
+        public int Index { get; } = index;
+
+        public bool IsEmpty => Parent == null;
+        public ref L Value => ref Parent.GetLeaf(Index);
+
+        public bool Equals(Leaf other)
+        {
+            return Parent == other.Parent && Index == other.Index;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Parent?.GetHashCode() ?? 0, Index);
+        }
     }
 
-    public sealed class Leaf : Node
+    public abstract class Branch
     {
-        public L? Value;
-    }
-
-    public sealed class Branch : Node
-    {
-        private NodeArray _nodes;
         public B? Value;
-
-        internal ref Node? GetNode(int index) => ref _nodes[index];
     }
 
-    [InlineArray(8)]
-    [StructLayout(LayoutKind.Sequential)]
-    public struct NodeArray
+    public sealed class LeafBranch : Branch
     {
-        private Node? _e0;
+        private Array _leaves;
+
+        public ref L GetLeaf(int index) => ref _leaves[index];
+
+        [InlineArray(8)]
+        private struct Array
+        {
+            private L _e0;
+        }
+    }
+
+    public sealed class NestBranch : Branch
+    {
+        private Array _branches;
+
+        public ref Branch? GetBranch(int index) => ref _branches[index];
+
+        [InlineArray(8)]
+        private struct Array
+        {
+            private Branch? _e0;
+        }
     }
 }
--- test/VoxelPizza.World.Octree.Test/OctreeTests.cs
+++ test/VoxelPizza.World.Octree.Test/OctreeTests.cs
@@ -1,19 +1,21 @@
+using System.Runtime.CompilerServices;
 using Xunit;
 
 namespace VoxelPizza.World.Octree.Test;
 
 public class OctreeTests
 {
-    public static TheoryData<int> DepthData => new(
-    [
-        0, 1, 2, 3, 4,
-    ]);
+    private static int[] _depthData = [1, 2, 3, 4];
+
+    public static TheoryData<int> DepthDataWithoutZero => new(_depthData);
+
+    public static TheoryData<int> DepthData => new([0, .. _depthData]);
 
     [Theory, MemberData(nameof(DepthData))]
-    public void Insert(int depth)
+    public void AddThenGet(int depth)
     {
         Octree<float, float> tree = new(depth);
-        int size = tree.Width;
+        int size = tree.GetWidth();
 
         for (int y = 0; y < size; y++)
         {
@@ -21,10 +23,30 @@ public class OctreeTests
             {
                 for (int x = 0; x < size; x++)
                 {
-                    var iLeaf = tree.GetOrAdd(x, y, z);
-                    var gLeaf = tree.Get(x, y, z);
+                    var iLeaf = tree.GetOrAddLeaf(x, y, z);
+                    Assert.False(iLeaf.IsEmpty);
 
+                    var gLeaf = tree.GetLeaf(x, y, z);
                     Assert.Equal(iLeaf, gLeaf);
+                }
+            }
+        }
+    }
+
+    [Theory, MemberData(nameof(DepthData))]
+    public void NullGet(int depth)
+    {
+        Octree<float, float> tree = new(depth);
+        int size = tree.GetWidth();
+
+        for (int y = 0; y < size; y++)
+        {
+            for (int z = 0; z < size; z++)
+            {
+                for (int x = 0; x < size; x++)
+                {
+                    var gLeaf = tree.GetLeaf(x, y, z);
+                    Assert.True(gLeaf.IsEmpty);
                 }
             }
         }
